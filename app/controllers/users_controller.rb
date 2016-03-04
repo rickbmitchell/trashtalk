9c51740ea4ca3647f201e3a5263ee53ac9106cee
Rick Mitchell <rickbmitchell@gmail.com]>
2016-03-04 11:48:58 -0500

    profiles and users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   end
 
   def new
-    @users = User.new
+    @user = User.new
   end
 
   def edit
@@ -15,16 +15,18 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      current_user
       flash[:notice] = "Welcome!"
-      redirect_to user_path @user
+      redirect_to login_path
     else
       flash[:alert] = "Your team sucks!"
-      redirect_to 'user/new'
+      redirect_to 'users/new'
     end
   end
 
   def show
     @user = get_user
+    @profile = get_profile
   end
 
   def destroy
@@ -32,13 +34,17 @@ class UsersController < ApplicationController
     @user.destroy
     session[:user_id] = nil
     flash[:notice] = "Your account was deleted"
-    redirect_to users_path
+    redirect_to '/'
   end
 
   private
 
   def get_user
     User.find(params[:id])
+  end
+
+  def get_profile
+    Profile.find(params[:id])
   end
 
 end
