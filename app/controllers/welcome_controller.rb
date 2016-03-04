ebc33cce513bf002ef26efbbf0489a1d8eaee6d8
sharpattack <sharp1209@gmail.com>
2016-03-04 15:15:11 -0500

    showed the ten most recents blogs on welcome#index page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 
   def index
-    @posts = Post.find(:all, :limit => 10, :order => 'created_at', :include => :username)
+    @posts = Post.order('created_at desc').limit(10)
   end
 
 end
